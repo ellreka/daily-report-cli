0a2d08b8255d3fbe3fe8167210492f83240e7747
ellreka <ellreka5364@gmail.com>
2020-06-16 09:00:19 +0900

    wip

--- app.py
+++ app.py
@@ -17,19 +17,51 @@ def main():
     togglApi = TogglAPI()
     slackApi = SlackAPI()
     argparser = argparse.ArgumentParser(description='daily-report-cli')
-    argparser.add_argument('-m', '--message', type=str,
-                           nargs='+', action='append', help='')
+    subparsers = argparser.add_subparsers(dest='subparser_name')
+    subparsers.required = False
+    current_parser = subparsers.add_parser('current', help='')
+    current_parser.set_defaults(func=current)
+    argparser.add_argument('-m', '--message', type=str, 
+    nargs='+', action='append', default=[], required=False, help='')
+    argparser.add_argument('-n', '--next', type=str,
+    nargs='+', action='append', default=[], required=False, help='')
+    argparser.add_argument('-d', '--did', type=str,
+    default=[], required=False, help='')
+    argparser.add_argument('-t', '--times', default=False,
+    type=bool, required=False, help='')
     args = argparser.parse_args()
-    message = '\n'.join(sum(args.message, []))
-    today = datetime.date.today()
-    activity = togglApi.get_details(today)
-    total = togglApi.get_summary(today)
-    content = "【やったこと】\nActivity:\n{}\n\nTotal:\n{}【思ったこと】\n{}\n\n\n【次回やること】".format(
-        activity, total, message)
-    slackApi.post_slack(content, today)
-    togglApi.get_summary(today)
-    current = togglApi.get_current_entry()
-    slackApi.update_status(current)
+    if hasattr(args, 'func'):
+        args.func()
+    else:
+        print('サブコマンドを指定して下さい')
+    # message = '\n'.join(sum(args.message, []))
+    # next = '\n'.join(sum(args.next, []))
+    # today = datetime.date.today()
+
+    # if args.did == 'toggl':
+    #     activity = togglApi.get_details(today)
+    #     total = togglApi.get_summary(today)
+    #     did = "\nActivity:\n{}\n\nTotal:\n{}".format(activity, total)
+    # else:
+    #     did = args.did
+
+    # content = "【やったこと】\n{}\n\n\n【思ったこと】\n{}\n\n\n【次回やること】\n{}".format(
+    #     did, message, next)
+    # slackApi.post_daily_report(content, today)
+    # if args.times:
+    #     slackApi.post_times(next, today)
+    # togglApi.get_summary(today)
+    # if current:
+    #     print('current')
+    #     current_entry = togglApi.get_current_entry()
+    #     slackApi.update_status(current_entry)
+
+def current():
+    print('current!!')
+    togglApi = TogglAPI()
+    slackApi = SlackAPI()
+    current_entry = togglApi.get_current_entry()
+    slackApi.update_status(current_entry)
 
 
 class TogglAPI:
@@ -47,8 +79,8 @@ class TogglAPI:
             'order_desc': 'off'
         }
         r = requests.get('https://toggl.com/reports/api/v2/details',
-                         auth=HTTPBasicAuth(self.api_token, 'api_token'),
-                         params=params)
+        auth=HTTPBasicAuth(self.api_token, 'api_token'),
+        params=params)
         json_r = r.json()
 
         def get_activities(n):
@@ -75,8 +107,8 @@ class TogglAPI:
             'until': date,
         }
         r = requests.get('https://toggl.com/reports/api/v2/summary',
-                         auth=HTTPBasicAuth(self.api_token, 'api_token'),
-                         params=params)
+        auth=HTTPBasicAuth(self.api_token, 'api_token'),
+        params=params)
         json_r = r.json()
 
         def get_total(n):
@@ -94,14 +126,14 @@ class TogglAPI:
             item_text = ""
             for i in t['items']:
                 item_text += "・{} ({})\n".format(i['title'],
-                                                 datetime.timedelta(milliseconds=i['time']))
+                datetime.timedelta(milliseconds=i['time']))
             total_text += "{} ({})\n{}\n\n".format(
                 t['project_name'], datetime.timedelta(milliseconds=t['time']), item_text)
         return total_text
 
     def get_current_entry(self):
         r = requests.get('https://www.toggl.com/api/v8/time_entries/current',
-                         auth=HTTPBasicAuth(self.api_token, 'api_token'))
+            auth=HTTPBasicAuth(self.api_token, 'api_token'))
         json_r = r.json()
         return json_r['data']
 
@@ -110,8 +142,9 @@ class SlackAPI:
     def __init__(self):
         self.token = os.environ.get('slack_token')
         self.channel = os.environ.get('slack_channel')
+        self.times_channel = os.environ.get('slack_times_channel')
 
-    def post_slack(self, content, today):
+    def post_daily_report(self, content, today):
         client = WebClient(self.token)
         response = client.files_upload(
             channels=self.channel,
@@ -119,6 +152,15 @@ class SlackAPI:
             content=content,
             filetype='post')
         assert response["file"]
+
+    def post_times(self, text, today):
+        print(self.times_channel)
+        print(text)
+        client = WebClient(self.token)
+        response = client.chat_postMessage(
+            channel=self.times_channel,
+            text="```{}```".format(text))
+        assert response
 
     def update_status(self, current):
         description = current['description'] if current is not None else ''
